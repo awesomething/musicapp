7622a4d489a553c1597a11ad317e1253a018edff
awesomething <79168736+PairApp@users.noreply.github.com>
2022-11-21 18:36:24 -0500

    used usecontext

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+function SearchBar(props) {
+    let [searchTerm, setSearchTerm] = useState('')
+  return (
+    <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
+        <input type="text" 
+        placeholder='Enter Search Term'
+        onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <input type='submit'/>
+    </form>
+  )
+}
+
+export default SearchBar
